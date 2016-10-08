468ff1c911b41671dd5ddd9e4875bd21d4bd7651
Don Maxwell Carlo II <solvedparadox@Dons-MacBook-Pro.local>
2016-10-07 22:39:09 -0700

    excluded object construction from for loop

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 var name = prompt('What is your name?')
 alert('Welcome to my site ' + name + '!')
 
-for (i = 0; i < 1 || i < question.length; i++) {
+  var question = ['Do I watch football?',
+  'Have I ever eatin sushi?',
+  'Is my nickname "Brogrammer"?',
+  'Do I like "Red Robin"?',
+  'Do I have any pets?',
+  'Have I ever watched \'Indiana Jones\'?',
+  'What is my favorite number?']
 
-  var q = new Object()
+  var answer = [
+    ['yes', 'no'],
+    ['no', 'yes'],
+    ['no', 'yes'],
+    ['yes', 'no'],
+    ['no', 'yes'],
+    ['no', 'yes'],
+    ['3', 'not 3']
+  ]
 
-  q.one = 'Do I watch football?'
-  q.two = 'Have I ever eatin sushi?'
-  q.three = 'Is my nickname "Brogrammer"?'
-  q.four = 'Do I like "Red Robin"?'
-  q.five = 'Do I have any pets?'
-  q.six = 'Have I ever watched \'Indiana Jones\'?'
-  q.seven = 'What is my favorite number?'
-
-  var question = [q.one, q.two, q.three, q.four, q.five, q.six, q.seven]
-  var answer = ['yes', 'no', 'no', 'yes', 'no', 'no', '3']
-  var notanswer = ['no', 'yes', 'yes', 'no', 'yes', 'yes', 'not 3']
+  for (i = 0; i < 1 || i < question.length; i++) {
 
   question[i] = prompt(question[i])
 
   if (i === 6) {
-    if (question[i] !== answer[i]) {
-      question[i] = notanswer[i];
+    if (question[i] !== answer[i][0]) {
+      question[i] = answer[i][1];
     }
   }
-    if (question[i] === answer[i]) {
+    if (question[i] === answer[i][0]) {
       alert('You\'re right.')
-    } else if (question[i] === notanswer[i]) {
+    } else if (question[i] === answer[i][1]) {
     alert('Nope.')
       i -= 1
     } else {
